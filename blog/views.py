e90bab03374241e0baa1a0ab350693b8e98373f7
kim1597 <yhkim1597@gmail.com>
2019-02-14 13:53:42 +0900

    사원증 응답뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -89,12 +89,13 @@ def naver_blog_search(request):
 
 
 def 사원증_이미지_응답(request):
-    ttf_path = 'C:/Windows/Fonts/malgun.ttf' # 윈도우, 맥:
+    # ttf_path = 'C:/Windows/Fonts/malgun.ttf' # 윈도우, 맥:
     # ttf_path = '/Library/Fonts/AppleGothic.ttf'
 
     # FIXME: 각 운영체제에 맞게 경로를 설정해주세요.
     # 파일을 복사한 후에, 경로 확인 후에 적용해주세요.
     # ttf_path = 'assets/fonts/AppleGothic.ttf'
+    ttf_path = 'assets/fonts/malgun.ttf' # 윈도우
 
     text = request.GET.get('name', '김영환')
     # text = '이진석 (사번: 201900001)'
