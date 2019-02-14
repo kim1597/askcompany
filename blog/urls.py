57b8c9b26ec29ee7ec26288c82b0c65b4075f2b2
kim1597 <yhkim1597@gmail.com>
2019-02-14 10:53:54 +0900

    네이버 블로그 검색 추가

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from blog.views import hello_times
 from blog.views import index, hello_times
 from blog.views import articles_by_year
 from blog.views import naver_realtime_keywords
+from blog.views import naver_blog_search
 
 from django.urls import register_converter
 from blog.converters import FourDigitYearConverter
@@ -12,4 +13,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('blog/', inclulde('blog.urls'))
         path('naver/실시간검색어/', naver_realtime_keywords),
+	    path('naver/네이버블로그검색/', naver_blog_search),
 ]
